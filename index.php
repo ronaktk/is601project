ff58b2291ec060f60ef7d9c40a8555554308f411
Ronak Patel <ronaktk@gmail.com>
2017-04-14 16:02:46 -0400

    display login page

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
+//require('db_connection.php');
+$action = filter_input(INPUT_POST,"action");
+
+if($action == NULL) {
+  $action = "display_login_page";
+}
+
+if($action == "display_login_page") {
+  include('view/login_page.php');
+}
+
 ?>
