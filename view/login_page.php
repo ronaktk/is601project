811d2e88e100a1047d7fc578d4ef382cd81a2853
Ronak Patel <ronaktk@gmail.com>
2017-04-14 16:10:29 -0400

    displayed register account button

--- view/login_page.php
+++ view/login_page.php
@@ -4,12 +4,21 @@
     <title>To-Do List</title>
   </head>
     <body>
-      <form method="post" action="index.php">
-        <input type="text" name="user_name" placeholder="Enter your name">
-	<input type="password" name="user_password" placeholder="Enter your
-	password">
-	<input type="hidden" name="action" value="check_user">
-	<input type="submit" value="Login">
-      </form>
+      <div>
+        <form method="post" action="index.php">
+          <input type="text" name="user_name" placeholder="Enter your name">
+	  <input type="password" name="user_password" placeholder="Enter your
+	  password">
+	  <input type="hidden" name="action" value="check_user">
+	  <input type="submit" value="Login">
+        </form>
+      </div>
+      <div>
+        <form method="post" action="index.php">
+	  <p>Don't have an account?</p>
+          <input type="hidden" name="action" value="register_user">
+          <input type="submit" value="Register">
+        </form>
+      </div>
     </body>
 </html>
