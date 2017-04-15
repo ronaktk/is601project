24faa196c3cd6e6c0ee08a7c6e9dce03bc9ed1c8
Ronak Patel <ronaktk@gmail.com>
2017-04-15 02:21:57 -0400

    added action to register user

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-//require('db_connection.php');
+require('model/db_connection.php');
+require('model/functions.php');
 $action = filter_input(INPUT_POST,"action");
 
 if($action == NULL) {
@@ -8,6 +9,20 @@ if($action == NULL) {
 
 if($action == "display_login_page") {
   include('view/login_page.php');
+}
+
+if($action == "register_user") {
+  $firstname = filter_input(INPUT_POST,'first_name');
+  $lastname = filter_input(INPUT_POST,'last_name');
+  $username = filter_input(INPUT_POST,'user_name');
+  $userpass = filter_input(INPUT_POST,'user_password');
+  $useremail = filter_input(INPUT_POST,'user_email');
+  $exit = registerUser($firstname,$lastname,$username,$userpass,$useremail);
+  if($exit == true) {
+    include('view/error1.php');
+  } else {
+    header("Location: view/login_page.php");
+  }
 }
 
 ?>
